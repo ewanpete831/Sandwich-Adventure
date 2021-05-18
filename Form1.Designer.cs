fadae53d04148201d0710db4dcb19f00e3d8e4f1
ewanpete831 <ewanpete831@outlook.com>
2021-05-18 10:02:03 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace Sandwich_Adventure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.option3Button = new System.Windows.Forms.Button();
+            this.button1Label = new System.Windows.Forms.Label();
+            this.button2Label = new System.Windows.Forms.Label();
+            this.button3Label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(75, 135);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(551, 261);
+            this.imageBox.TabIndex = 0;
+            this.imageBox.TabStop = false;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(12, 18);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(681, 87);
+            this.outputLabel.TabIndex = 1;
+            // 
+            // option1Button
+            // 
+            this.option1Button.BackgroundImage = global::Sandwich_Adventure.Properties.Resources.Button_Next_icon;
+            this.option1Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.option1Button.FlatAppearance.BorderSize = 0;
+            this.option1Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.option1Button.Location = new System.Drawing.Point(75, 412);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(74, 51);
+            this.option1Button.TabIndex = 2;
+            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // option2Button
+            // 
+            this.option2Button.BackgroundImage = global::Sandwich_Adventure.Properties.Resources.Button_Next_icon;
+            this.option2Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.option2Button.FlatAppearance.BorderSize = 0;
+            this.option2Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.option2Button.Location = new System.Drawing.Point(75, 469);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(74, 51);
+            this.option2Button.TabIndex = 3;
+            this.option2Button.UseVisualStyleBackColor = true;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // option3Button
+            // 
+            this.option3Button.BackgroundImage = global::Sandwich_Adventure.Properties.Resources.Button_Next_icon;
+            this.option3Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.option3Button.FlatAppearance.BorderSize = 0;
+            this.option3Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.option3Button.Location = new System.Drawing.Point(75, 526);
+            this.option3Button.Name = "option3Button";
+            this.option3Button.Size = new System.Drawing.Size(74, 51);
+            this.option3Button.TabIndex = 4;
+            this.option3Button.UseVisualStyleBackColor = true;
+            this.option3Button.Click += new System.EventHandler(this.option3Button_Click);
+            // 
+            // button1Label
+            // 
+            this.button1Label.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1Label.Location = new System.Drawing.Point(155, 412);
+            this.button1Label.Name = "button1Label";
+            this.button1Label.Size = new System.Drawing.Size(471, 51);
+            this.button1Label.TabIndex = 5;
+            // 
+            // button2Label
+            // 
+            this.button2Label.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2Label.Location = new System.Drawing.Point(151, 479);
+            this.button2Label.Name = "button2Label";
+            this.button2Label.Size = new System.Drawing.Size(475, 51);
+            this.button2Label.TabIndex = 6;
+            // 
+            // button3Label
+            // 
+            this.button3Label.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3Label.Location = new System.Drawing.Point(151, 526);
+            this.button3Label.Name = "button3Label";
+            this.button3Label.Size = new System.Drawing.Size(484, 51);
+            this.button3Label.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 587);
+            this.Controls.Add(this.button3Label);
+            this.Controls.Add(this.button2Label);
+            this.Controls.Add(this.button1Label);
+            this.Controls.Add(this.option3Button);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.imageBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Journey to the Sandwich Store";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Button option3Button;
+        private System.Windows.Forms.Label button1Label;
+        private System.Windows.Forms.Label button2Label;
+        private System.Windows.Forms.Label button3Label;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sandwich_Adventure
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+        Random randNumber = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void option3Button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void displayPage()
+        {
+            switch(page)
+            {
+                case 1:
+                    outputLabel.Text = "You are sitting at home, it is almost lunch time.";
+                    option3Button.Visible = false;
+                    button1Label.Text = "Go to the sandwich store for lunch";
+                    button2Label.Text = "Stay at home for lunch";
+                    break;
+
+                case 2:
+                    outputLabel.Text = "You step outside, and realize it is very cold out!";
+                    option3Button.Visible = true;
+                    button1Label.Text = "Go inside and get a jacket";
+                    button2Label.Text = "Call a Taxi";
+                    button3Label.Text = "Continue walking in your T-Shirt";
+                    break;
+
+                case 3:
+                    outputLabel.Text = "You starve to death, as you have no food at home. Play again?";
+                    option3Button.Visible = false;
+                    button1Label.Text = "Yes";
+                    button2Label.Text = "No";
+                    break;
+
+                case 4:
+                    outputLabel.Text = "You put on a jacket, and start walking to the sandwich store." +
+                        " You know a shortcut through the woods, but it's icy out and may be dangerous";
+                    option3Button.Visible = false;
+                    button1Label.Text = "Take the shortcut";
+                    button2Label.Text = "Take the long way";
+                    break;
+
+                case 5:
+                    outputLabel.Text = "The Taxi doesn't arrive fast enough, you starve to death! Play again?";
+                    option3Button.Visible = false;
+                    button1Label.Text = "Yes";
+                    button2Label.Text = "No";
+                    break;
+
+                case 6:
+                    outputLabel.Text = "The Taxi arrives very quickly and asks where you would like to be taken";
+                    option3Button.Visible = false;
+                    button1Label.Text = "Go to the nearest sandwich store";
+                    button2Label.Text = "Go to the town centre";
+                    break;
+
+                case 7:
+                    outputLabel.Text = "You begin walking to the sandwich shop, but quickly realize it was a lot colder than you thought." +
+                        " You Freeze to death.Play Again?";
+                    option3Button.Visible = false;
+                    button1Label.Text = "Yes";
+                    button2Label.Text = "No";
+                    break;
+
+                case 8:
+                    outputLabel.Text = "You make it through the woods unharmed, on the other side, there are 2 paths into the city.";
+                    button1Label.Text = "Cut through the alleyway";
+                    button2Label.Text = "Take the long way by the main road";
+                    break;
+
+                case 9:
+                    outputLabel.Text = "You Slip on some ice while taking the shortcut, you hit your head and die. Play again?";
+                    button1Label.Text = "Yes";
+                    button2Label.Text = "No";
+                    break;
+
+                case 10:
+                    outputLabel.Text = "You decide to take the long way into the city, but on your way there," +
+                        " part of the sidewalk is closed for construction. You try to cross the street to avoid it," +
+                        " but you forgot to look both ways before crossing. You are hit by a fast-moving truck, and die. Play again?";
+                    button1Label.Text = "Yes";
+                    button2Label.Text = "No";
+                    break;
+
+                case 11:
+                    outputLabel.Text = "While walking through the dark alleyway, you are suddenly surrounded by the calm sound of a piano," +
+                        " you stop to listen for a moment, but regret it immediately as you see the piano falling from a building" +
+                        ", and promptly crushing you. Play again?";
+                    button1Label.Text = "Yes";
+                    button2Label.Text = "No";
+                    break;
+
+                case 12:
+                    outputLabel.Text = "While walking down the main road, you see a beggar asking for money.";
+                    button1Label.Text = "Give the begger $10";
+                    button2Label.Text = "Ignore them";
+                    break;
+
+                case 13:
+                    outputLabel.Text = "Shortly after giving the beggar money, you realize you will no longer be able to afford a sandwich";
+                    button1Label.Text = "Steal the money back";
+                    button2Label.Text = "Ask for the money back nicely";
+                    break;
+
+                case 14:
+                    outputLabel.Text = "You made it to the sandwich store! You get your lunch and are full for the rest of the day. Play again ? ";
+                    option3Button.Visible = false;
+                    option2Button.Visible = true;
+                    button1Label.Text = "Yes";
+                    button2Label.Text = "No";
+                    break;
+
+                case 15:
+                    outputLabel.Text = "A bystander catches you stealing from the beggar, you are arrested and starve to death in the long ride to the jail. Play again?";
+                    button1Label.Text = "Yes";
+                    button2Label.Text = "No";
+                    break;
+
+                case 16:
+                    outputLabel.Text = "The beggar gets angry at you for trying to take your money back, he stabs you and takes the rest of your money. Play again?";
+                    button1Label.Text = "Yes";
+                    button2Label.Text = "No";
+                    break;
+
+                case 17:
+                    outputLabel.Text = "The driver drops you off in the center of the town. You know 3 paths from here to the sandwich store.";
+                    option3Button.Visible = true;
+                    button1Label.Text = "Take the side street";
+                    button2Label.Text = "Take the main road West";
+                    button3Label.Text = "Take the road North";
+                    break;
+
+                case 18:
+                    outputLabel.Text = "Unfortunately, the driver doesn't know the town very well," +
+                        " and ends up driving in circles for hours. You die of starvation. Play again?";
+                    button1Label.Text = "Yes";
+                    button2Label.Text = "No";
+                    break;
+
+                case 19:
+                    outputLabel.Text = "You come up to a construction site.";
+                    option3Button.Visible = false;
+                    button1Label.Text = "Walk around it";
+                    button2Label.Text = "Walk through the construction site";
+                    break;
+
+                case 20:
+                    outputLabel.Text = "You begin walking West, but you take a wrong turn, and realize you are lost.";
+                    option3Button.Visible = false;
+                    button1Label.Text = "Ask someone for directions";
+                    button2Label.Text = "Keep walking";
+                    break;
+
+                case 21:
+                    outputLabel.Text = "You walk around the construction site and see two roads to the sandwich store.";
+                    button1Label.Text = "Take the shortest road";
+                    button2Label.Text = "Take the longer road";
+                    break;
+
+                case 22:
+                    outputLabel.Text = "While cutting through the construction zone, a hammer falls from above," +
+                        " and hits you on the head. you die. Play again?";
+                    button1Label.Text = "Yes";
+                    button2Label.Text = "No";
+                    break;
+
+                case 23:
+                    outputLabel.Text = "You spend 2 hours looking for someone to give you directions, but nobody knows where the sandwich store is." +
+                        " You starve to death. Play again?";
+                    button1Label.Text = "Yes";
+                    button2Label.Text = "No";
+                    break;
+
+                case 24:
+                    outputLabel.Text = "You realize you weren't actually lost, and there was just a new building on this road.";
+                    option2Button.Visible = false;
+                    button1Label.Text = "Next";
+                    break;
+                        
+
+
+
+            }
+        }
+      
+    }
+}
